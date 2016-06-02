a1f1e6f5effa0c01d19451b577dcfabd33dbdb4f
Fagerberg90 <Fagerberg_90@hotmail.com>
2016-06-02 03:29:49 +0200

    fixed smiley and js

--- Labb4/scripts/emoji.js
+++ Labb4/scripts/emoji.js
@@ -1,1 +1,21 @@
-﻿
+﻿$(document).ready(function () {
+    $("select").change(function () {
+        $(this).find("option:selected").each(function () {
+            if ($(this).attr("value") == "happy") {
+                $(".box").not(".happy").hide();
+                $(".happy").show();
+            }
+            else if ($(this).attr("value") == "good") {
+                $(".box").not(".good").hide();
+                $(".good").show();
+            }
+            else if ($(this).attr("value") == "sad") {
+                $(".box").not(".sad").hide();
+                $(".sad").show();
+            }
+            else {
+                $(".box").hide();
+            }
+        });
+    }).change();
+});
